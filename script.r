db685e5498ffc1d4f9a0293ce0bb3e86a5684c2c
Caleb Smith <karagenit@outlook.com>
2018-05-03 21:48:54 -0400

    Add Linear Model Function

--- script.r
+++ script.r
@@ -1,17 +1,20 @@
 #!/usr/bin/env Rscript
 
+linearModel <- function(dataset) {
+    lsrl = lm(dataset$VOLT ~ dataset$TOTAL.AMP)
+
+    x11()
+    plot(dataset$TOTAL.AMP, dataset$VOLT, col = "blue")
+    abline(lsrl, col = "green")
+
+    return(lsrl)
+}
+
 data4cim = read.csv("data/4cim.csv", header = TRUE)
+data6cim = read.csv("data/6cim.csv", header = TRUE)
 
-print(data4cim$VOLT)
-print(data4cim$TOTAL.AMP)
-correl = cor(data4cim$TOTAL.AMP, data4cim$VOLT)
-lsrl = lm(data4cim$VOLT ~ data4cim$TOTAL.AMP)
-print(correl)
-print(lsrl)
-
-x11()
-plot(data4cim$TOTAL.AMP, data4cim$VOLT, col = "blue")
-abline(lsrl, col = "green")
+linearModel(data4cim)
+linearModel(data6cim)
 
 # Residuals Plot
 #x11()
