13302f726fe22130053693187ffac61eaab063c3
Caleb Smith <karagenit@outlook.com>
2018-05-03 21:35:50 -0400

    Add Correlation Calculations

--- script.r
+++ script.r
@@ -4,6 +4,10 @@ data4cim = read.csv("data/4cim.csv", header = TRUE)
 
 print(data4cim$VOLT)
 print(data4cim$TOTAL.AMP)
+correl = cor(data4cim$TOTAL.AMP, data4cim$VOLT)
+lsrl = lm(data4cim$TOTAL.AMP ~ data4cim$VOLT)
+print(correl)
+print(lsrl)
 
 x11()
 plot(data4cim$TOTAL.AMP, data4cim$VOLT, col = "blue")
